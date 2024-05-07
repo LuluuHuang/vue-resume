84ecf0e806531ab8715be7eb62c5bbfbe2e3430f
Lulu <yuruh18@gmail.com>
2024-05-07 11:06:27 +0800

    0507

--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -14,7 +14,8 @@
         <div class="col-lg-4 col-md-6 col-sm-6 col-12 animate__animated animate__fadeInUp animate__delay-1s">
             <p class="fs-3 btnStyle">工作經歷</p >
             <ul v-for="(exp,i) in infos.experience" class="exp" :key="i">
-                <li>{{exp}}</li>
+                <li>{{exp.time}}</li>
+                <li>{{exp.job}}</li>
             </ul>
             <p class="fs-3 btnStyle">學歷</p>
             <ul class="edu">
@@ -41,9 +42,18 @@
                         'Bootstrap',
                     ],
                     experience:[
-                        '長林創新股份有限公司 前端工程師',
-                        '汨宇國際有限公司 行銷專員',
-                        '霧峰區戶政事務所 行政助理'
+                        {
+                            job:'長林創新股份有限公司 前端工程師',
+                            time:'2023/2~今'
+                        },
+                        {
+                            job:'汨宇國際有限公司 行銷專員',
+                            time:'2022/3~2022/8'
+                        },
+                        {
+                            job:'霧峰區戶政事務所 行政助理',
+                            time:'2020/2~2021/10'
+                        }                        
                     ],
                     educate:'世新大學 公共關係暨廣告學系 畢業'
                 }
